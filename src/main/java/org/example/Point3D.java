eb458c4076642fac958128ef2e2cb76a1962e10f
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-10-15 01:42:55 +0100

    Added 3D Point class

---
+++ src/main/java/org/example/Point3D.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Point3D {
+    private int x;
+    private int y;
+    private int z;
+
+    public Point3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() { return z; }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point3D p = (Point3D) o;
+
+        return x == p.x && y == p.y && z == p.z;
+    }
+}
